85385d36756d8a4c040455750dc11310cf22b80a
Aldiramadhan271 <ramadhanaldi60@gmail.com>
2022-07-15 13:18:19 +0700

    first commit

---
+++ transisi.js
@@ -0,0 +1,9 @@
+//need to make a function that gets rid of the preload
+
+
+window.addeventistener('load', ) ( {
+	const function (preload) {
+		return document.queryselector('preload');
+	},
+preload,classlist,add('.preload - finish');
+}); 
